68822dd822bbcd2b8842a91cdf6d4d9ca7c6cd4f
Tao <werewaswas@live.com>
2018-07-06 02:00:29 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,7 +23,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board)
+def turn(board, player)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
@@ -44,7 +44,7 @@ def play(board)
     else
       player = 'O'
     end
-    turn(board)
+    turn(board, player)
     i += 1
   end
 end
